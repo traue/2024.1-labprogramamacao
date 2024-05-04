c27e7e7798ba21b2365319d3092c766e1f9efd9f
Thiago G. Traue <t.traue@gmail.com>
2024-05-04 15:01:37 -0300

    Correção do código

--- 6_locadora_veiculos/src/model/Veiculo.java
+++ 6_locadora_veiculos/src/model/Veiculo.java
@@ -1,12 +1,104 @@
 package model;
 
 public class Veiculo {
-	public Fabricante fabricante;
-	public String cor;
-	public String modelo;
-	public String placa;
-	public int numPortas;
-	public double kilometragem;
-	public double valorDiaria;
-	public TipoCambio tipoCambio;
+
+	private String fabricante;
+	private String modelo;
+	private int anoFabricacao;
+	private String cor;
+	private String placa;
+	private int numPortas;
+	private double kilometragem;
+	private double valorDiaria;
+	private TipoCambio tipoCambio;
+
+	public Veiculo(String fabricante, String cor, String modelo, int anoFabricacao, String placa, int numPortas,
+			double kilometragem, double valorDiaria, TipoCambio tipoCambio) {
+		this.fabricante = fabricante;
+		this.cor = cor;
+		this.modelo = modelo;
+		this.anoFabricacao = anoFabricacao;
+		this.placa = placa;
+		this.numPortas = numPortas;
+		this.kilometragem = kilometragem;
+		this.valorDiaria = valorDiaria;
+		this.tipoCambio = tipoCambio;
+	}
+
+	public Veiculo() {
+
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+	public void setAnoFabricacao(int anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getNumPortas() {
+		return numPortas;
+	}
+
+	public void setNumPortas(int numPortas) {
+		this.numPortas = numPortas;
+	}
+
+	public double getKilometragem() {
+		return kilometragem;
+	}
+
+	public void setKilometragem(double kilometragem) {
+		this.kilometragem = kilometragem;
+	}
+
+	public double getValorDiaria() {
+		return valorDiaria;
+	}
+
+	public void setValorDiaria(double valorDiaria) {
+		this.valorDiaria = valorDiaria;
+	}
+
+	public TipoCambio getTipoCambio() {
+		return tipoCambio;
+	}
+
+	public void setTipoCambio(TipoCambio tipoCambio) {
+		this.tipoCambio = tipoCambio;
+	}
+
 }
--- 6_locadora_veiculos/src/test/Principal.java
+++ 6_locadora_veiculos/src/test/Principal.java
@@ -3,7 +3,7 @@ package test;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import model.Fabricante;
+import model.TipoCambio;
 import model.Veiculo;
 
 public class Principal {
@@ -11,57 +11,85 @@ public class Principal {
 	public static void main(String[] args) {
 
 		ArrayList<Veiculo> veiculos = new ArrayList<>();
-		int opc = imprimeMenu();
 
-		do {
-			switch (opc) {
-			case 1:
-				veiculos.add(cadastraVeiculo());
-				break;
-			case 2:
-				// inicia o cadastro do cliente
-				break;
-			default:
-				break;
+		try (Scanner sc = new Scanner(System.in)) {
+
+			while (true) {
+
+				imprimeMenu();
+				System.out.print("\n>> Sua escolha: ");
+				int opc = sc.nextInt();
+
+				switch (opc) {
+				case 1:
+					veiculos.add(novoVeiculo(sc));
+					break;
+				case 2:
+					// inicia o cadastro do cliente
+					break;
+				case 0:
+					System.out.println("Até a próxima!");
+					sc.close();
+					System.exit(0);
+					break;
+				default:
+					break;
+				}
 			}
-		} while (imprimeMenu() != 0);
+		}
 
 	}
 
-	private static int imprimeMenu() {
-		Scanner sc = new Scanner(System.in);
+	private static Veiculo novoVeiculo(Scanner sc) {
+		Veiculo v = new Veiculo();
+		sc.nextLine();
+		System.out.println("Informe os dados do veículo a ser cadastrado:");
 
+		System.out.print("Fabricante: ");
+		v.setFabricante(sc.nextLine());
+
+		System.out.print("Modelo: ");
+		v.setModelo(sc.nextLine());
+
+		System.out.print("Ano: ");
+		v.setAnoFabricacao(sc.nextInt());
+		sc.nextLine();
+
+		System.out.print("Cor: ");
+		v.setCor(sc.nextLine());
+
+		System.out.print("Placa: ");
+		v.setPlaca(sc.nextLine());
+
+		System.out.print("Nº de portas: ");
+		v.setNumPortas(sc.nextInt());
+		sc.nextLine();
+
+		System.out.print("Kilimetragem: ");
+		v.setKilometragem(sc.nextInt());
+		sc.nextLine();
+
+		System.out.print("Tipo de câmbio: [1] manual | [2] aumático");
+		int tipo = sc.nextInt();
+		sc.nextLine();
+		v.setTipoCambio(tipo == 1 ? TipoCambio.manual : TipoCambio.automatico);
+
+		System.out.print("Valor da diária: R$ ");
+		v.setValorDiaria(sc.nextFloat());
+
+		return v;
+	}
+
+	private static void imprimeMenu() {
 		System.out.println("****** MENU ******");
 		System.out.println("[1] - Cadastrar veículo");
 		System.out.println("[2] - Cadastrar cliente");
-		System.out.println("[3] - Realização locação");
-		System.out.println("[4] - Realização devolução");
+		System.out.println("[3] - Realizar locação");
+		System.out.println("[4] - Realizar devolução");
+		System.out.println("[5] - Listar Veículos");
+		System.out.println("[6] - Listar clientes");
 		System.out.println("[0] - Sair");
-		System.out.print("\n>> Sua escolha: ");
-		int opc = sc.nextInt();
-		sc.close();
-		return opc;
-	}
-
-	private static Veiculo cadastraVeiculo() {
-		Veiculo v = new Veiculo();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Informe os dados do veículo cadastrado:");
-
-//		for (Fabricante f : Fabricante.values()) {
-//			System.out.println(" - " + f);
-//		}
-
-		System.out.println("Fabricante: ");
-		String fab = sc.nextLine();
-		// sc.nextLine();
-		v.fabricante = Fabricante.valueOf(fab);
-
-		System.out.print("Cor: ");
-		v.cor = sc.nextLine();
-		// ...
-		sc.close();
-		return v;
+		System.out.println("************");
 	}
 
 }
